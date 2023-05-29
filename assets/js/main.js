8fd2281c9cb65fc7d5354b1aadf4d7dc869df608
GouthamRavichandran <112435398+GouthamRavichandran@users.noreply.github.com>
2023-05-29 00:19:20 -0400

    Update main.js for Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,14 +66,22 @@ tabs.forEach(tab => {
     })
 })
 
-/*==================== SERVICES MODAL ====================*/
-
-
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container',{
+    cssMode: true,
+    loop: true,
 
-
-/*==================== TESTIMONIAL ====================*/
-
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    mousewheel:true,
+    keyboard: true,
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
